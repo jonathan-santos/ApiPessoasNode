7aaa3bcb012348483ef276cc7c8100ca58e469d7
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2018-09-13 11:14:53 -0300

    Corrigindo a string de conexão do banco de dados e adicionando a route pessoas com um endpoint POST

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds018248.mlab.com:18248/testes');
+mongoose.connect('mongodb://john:a123456@ds018248.mlab.com:18248/testes', { useNewUrlParser: true });
 
 const port = process.env.PORT || 8080;
 const router = express.Router();
@@ -15,6 +15,19 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.json({ message: 'Olá, essa é uma api' });
 });
+
+router.route('/pessoas')
+    .post((req, res) => {
+        const pessoa = new Pessoa();
+        pessoa.nome = req.body.nome;
+
+        pessoa.save(erro => {
+            if(erro)
+                res.send(erro);
+
+            res.json({ message: 'Pessoa criada' });
+        });
+    });
 
 app.use('/api', router);
 
